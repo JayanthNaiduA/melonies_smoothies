8c78760ccb1093f6c2bca83341ab6a21c490a218
JayanthNaiduA <thejayanthnaidu@gmail.com>
2026-01-11 14:44:25 +0530

    Update dataframe handling to use Pandas
    Converted Snowpark DataFrame to Pandas DataFrame for display.

--- streamli_app.py
+++ streamli_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 import requests
 from snowflake.snowpark.functions import col
+import pandas as pd
 
 # Write directly to the app
 st.title(f":cup_with_straw: Costomize Your Smoothie! :cup_with_straw: ")
@@ -15,7 +16,10 @@ st.write("The Name on your Smoothie will be:", name_on_order)
 cnx = st.connection("snowflake")
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
-st.dataframe(data=my_dataframe, use_container_width=True)
+#st.dataframe(data=my_dataframe, use_container_width=True)
+# convert snowpark dataframe to pandas dataframe so we can use LOC function
+pd_df = my_dataframe.to_pandas()
+st.dataframe(pd_df)
 st.stop()
 
 ingredients_list = st.multiselect(
